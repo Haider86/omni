d8abeb23aa2259a12c52244cb92810cea0f1d798
Haider Alsalih <alsalihns@gmail.com>
2018-09-12 21:12:27 +0200

    added smooth scroll

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-
+    /*sticky navigation*/
     $('.js--section-features').waypoint(function (direction) {
         if (direction == "down") {
             $('nav').addClass('sticky');
@@ -10,11 +10,62 @@ $(document).ready(function () {
             offset: '60px'
         });
     
-    
-    
-    $('h1').click(function(){
-        $(this).css('color', 'ff0000');
+    //scroll down to section plan 
+    $('.js--scroll-to-plan').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 4000);
     });
+    
+    /* scroll down to section featuers*/
+    $('.js--scroll-to-start').click(function(){
+        $('html , body').animate({scrollTop: $('.js--section-features').offset().top}, 2000);
+    });
+    
+    // back to header
+    
+    $('.js--logo-black').click(function(){
+        $('html , body').animate({scrollTop: $('.js--logo').offset().top}, 2000);
+    });
+    
+    
+    
+    // Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+    
+    
 
 
 
